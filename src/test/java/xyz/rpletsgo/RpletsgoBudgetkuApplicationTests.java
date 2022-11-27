427804c1d145a54999ccf863ca24b8a9bf1a70db
Immanuel01 <immanuel01@ui.ac.id>
2022-11-27 15:46:19 +0700

    chore: set up failing unit tests

--- src/test/java/xyz/rpletsgo/RpletsgoBudgetkuApplicationTests.java
+++ src/test/java/xyz/rpletsgo/RpletsgoBudgetkuApplicationTests.java
@@ -3,11 +3,18 @@ package xyz.rpletsgo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 @SpringBootTest
 class RpletsgoBudgetkuApplicationTests {
     
     @Test
     void contextLoads() {
+    }
+    
+    @Test
+    void contextLoads2() {
+        assertSame(1, 2);
     }
     
 }
