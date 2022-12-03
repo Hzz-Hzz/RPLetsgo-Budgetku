52f7689451c0379227f1d9244c2f0d813dbcd682
Immanuel01 <immanuel01@ui.ac.id>
2022-12-04 00:11:07 +0700

    test: Implement WorkspaceControllerTest

---
+++ src/test/java/xyz/rpletsgo/workspace/controller/WorkspaceControllerTest.java
@@ -0,0 +1,58 @@
+package xyz.rpletsgo.workspace.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import xyz.rpletsgo.workspace.service.WorkspaceService;
+
+import static org.mockito.Mockito.*;
+
+class WorkspaceControllerTest {
+    
+    @MockBean
+    WorkspaceService workspaceService;
+    
+    @InjectMocks
+    WorkspaceController workspaceController;
+    
+    @BeforeEach
+    void setup(){
+        workspaceService = mock(WorkspaceService.class);
+        MockitoAnnotations.openMocks(this);
+    }
+    
+    String workspaceId = "a";
+    String workspaceName = "b";
+    
+    @Test
+    void createWorkspace() {
+        workspaceController.createWorkspace(workspaceName);
+        verify(workspaceService, times(1)).createWorkspace(workspaceName);
+    }
+    
+    @Test
+    void updateWorkspace() {
+        workspaceController.updateWorkspace(workspaceId, workspaceName);
+        verify(workspaceService, times(1)).updateWorkspace(workspaceId, workspaceName);
+    }
+    
+    @Test
+    void getWorkspaceSingle() {
+        workspaceController.getWorkspace(workspaceId);
+        verify(workspaceService, times(1)).getWorkspace(workspaceId);
+    }
+    
+    @Test
+    void getWorkspaceMultiple() {
+        workspaceController.getWorkspace();
+        verify(workspaceService, times(1)).getWorkspaces();
+    }
+    
+    @Test
+    void deleteWorkspace() {
+        workspaceController.deleteWorkspace(workspaceId);
+        verify(workspaceService, times(1)).deleteWorkspace(workspaceId);
+    }
+}
