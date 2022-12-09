867b260b446c8e5841b7098931d19837b99c3c49
azzam.labib <azzam.labib@ui.ac.id>
2022-12-10 06:51:43 +0700

    Add Overview Backend Utility

---
+++ src/main/java/xyz/rpletsgo/common/controller/OverviewController.java
@@ -0,0 +1,31 @@
+package xyz.rpletsgo.common.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import xyz.rpletsgo.common.service.CommonService;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/{workspaceId}/overview")
+public class OverviewController {
+    @Autowired
+    CommonService commonService;
+
+    @RequestMapping(method = RequestMethod.GET, path = "")
+    public String listTotalSummary(
+            @PathVariable String workspaceId,
+            Model model
+    ){
+        Map<String, String> totalFinancialSummary = commonService.getTotalFinancial(workspaceId);
+        model.addAttribute("totalPemasukan", totalFinancialSummary.get("pemasukan"));
+        model.addAttribute("totalPengeluaran", totalFinancialSummary.get("pengeluaran"));
+        model.addAttribute("totalTagihan", totalFinancialSummary.get("tagihan"));
+        return "common/overview";
+    }
+}
--- src/main/java/xyz/rpletsgo/common/service/CommonService.java
+++ src/main/java/xyz/rpletsgo/common/service/CommonService.java
@@ -6,7 +6,9 @@ import xyz.rpletsgo.auth.component.CurrentLoggedInPengguna;
 import xyz.rpletsgo.common.model.FinancialEvent;
 import xyz.rpletsgo.workspace.repository.WorkspaceRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class CommonService {
@@ -18,5 +20,46 @@ public class CommonService {
     public List<FinancialEvent> getWorkspaceFinancialEvents(String workspaceId){
         var workspace = loggedInPengguna.authorizeWorkspace(workspaceId);
         return workspace.getFinancialEvents();
+    }
+    
+    private long getTotalPemasukan(String workspaceId){
+        var workspace = loggedInPengguna.authorizeWorkspace(workspaceId);
+        List<FinancialEvent> pemasukanList = workspace.getPemasukans();
+        long totalPemasukan = 0;
+        for(FinancialEvent pemasukan : pemasukanList){
+            totalPemasukan += pemasukan.getNominal();
+        }
+        return totalPemasukan;
+    }
+
+    private long getTotalPengeluaran(String workspaceId){
+        var workspace = loggedInPengguna.authorizeWorkspace(workspaceId);
+        List<FinancialEvent> pengeluaranList = workspace.getPengeluarans();
+        long totalPengeluaran = 0;
+        for(FinancialEvent pengeluaran : pengeluaranList){
+            totalPengeluaran += pengeluaran.getNominal();
+        }
+        return totalPengeluaran;
+    }
+
+    private long getTotalTagihan(String workspaceId){
+        var workspace = loggedInPengguna.authorizeWorkspace(workspaceId);
+        List<FinancialEvent> tagihanList = workspace.getTagihan();
+        long totalTagihan = 0;
+        for(FinancialEvent tagihan : tagihanList){
+            totalTagihan += tagihan.getNominal();
+        }
+        return totalTagihan;
+    }
+
+    public Map<String, String> getTotalFinancial(String workspaceId){
+        Map<String, String> financialTotalSummary = new HashMap<>();
+        String pengeluaran = Long.toString(getTotalPengeluaran(workspaceId));
+        String pemasukan = Long.toString(getTotalPemasukan(workspaceId));
+        String tagihan = Long.toString(getTotalTagihan(workspaceId));
+        financialTotalSummary.put("pengeluaran", pengeluaran);
+        financialTotalSummary.put("pemasukan", pemasukan);
+        financialTotalSummary.put("tagihan", tagihan);
+        return financialTotalSummary;
     }
 }
